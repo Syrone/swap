99b5fe45abe9e272000a57651449988da088166b
Andrew Vasilchenko <cazonove@gmail.com>
2024-09-29 17:51:25 +0300

    fix

--- src/js/components/banner.js
+++ src/js/components/banner.js
@@ -4,6 +4,21 @@ banners?.forEach((element) => {
 	const buttonClose = element.querySelector('.banner-close')
 
 	buttonClose?.addEventListener('click', () => {
-		element.remove()
-	})
+    const currentHeight = element.scrollHeight + 'px'
+
+    element.style.height = currentHeight
+
+    setTimeout(() => {
+      element.style.transition = 'opacity 200ms ease, height 600ms ease, padding 600ms ease, margin 600ms ease'
+      element.style.opacity = '0'
+      element.style.height = '0px'
+      element.style.padding = '0px'
+      element.style.marginTop = '-1rem'
+    }, 10)
+
+    setTimeout(() => {
+			element.style.position = 'absolute'
+      element.remove()
+    }, 600)
+  })
 })
