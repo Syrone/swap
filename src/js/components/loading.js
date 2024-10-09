13e5c3c9252deccbd91f0cbbaff462b70e021c12
Andrew Vasilchenko <cazonove@gmail.com>
2024-10-09 14:37:50 +0300

    fix sceleton loading

--- src/js/components/loading.js
+++ src/js/components/loading.js
@@ -1,11 +1,42 @@
-function removeSkeleton() {
-  const skeletons = document.querySelectorAll('[data-loading-sceleton]')
+function removeSkeleton(element) {
+  setTimeout(() => {
+    element.removeAttribute('data-loading-sceleton');
+  }, 300);
+}
+
+function observeSkeletons() {
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (mutation.type === 'attributes' && mutation.attributeName === 'data-loading-sceleton') {
+        removeSkeleton(mutation.target);
+      } else if (mutation.type === 'childList') {
+        mutation.addedNodes.forEach((node) => {
+          if (node.nodeType === Node.ELEMENT_NODE) {
+            if (node.hasAttribute('data-loading-sceleton')) {
+              removeSkeleton(node);
+            }
+            node.querySelectorAll('[data-loading-sceleton]').forEach(removeSkeleton);
+          }
+        });
+      }
+    });
+  });
+
+  observer.observe(document.body, {
+    attributes: true,
+    attributeFilter: ['data-loading-sceleton'],
+    childList: true,
+    subtree: true
+  });
+}
+
+function removeInitialSkeletons() {
+  const skeletons = document.querySelectorAll('[data-loading-sceleton]');
   
-  skeletons.forEach(skeleton => {
-    skeleton.removeAttribute('data-loading-sceleton')
-  })
+  skeletons.forEach(removeSkeleton);
 }
 
 window.onload = function() {
-  removeSkeleton()
+  removeInitialSkeletons();
+  observeSkeletons();
 }
